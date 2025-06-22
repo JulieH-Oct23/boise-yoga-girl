2c4dc92c338dad8607e732e6eb3489825b388f71
Julie Hart <147504798+JulieH-Oct23@users.noreply.github.com>
2025-06-22 12:43:25 -0600

    color theme total update push

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ import styles from "./HomePage.module.css";
 const HomePage = () => (
   <main className="app-main">
     <section className={styles.introSection}>
-      <h2 className={styles.sectionTitle}>🌟 Welcome 🌟 </h2>
+      <h2 className={styles.sectionTitleWelcome}>🌟 Welcome 🌟 </h2>
       <p className={styles.introText}>
         This application has been designed to help yoga teachers reference poses, indications, cues, and create sequences where yoga teachers can keep notes, meditations, and playlists all in one location.
       </p>
